d2d5206aae19d0a2da4734220b5a1f51a8a61075
Ashwani Kumar <ashwanikumar04@gmail.com>
2018-11-10 23:41:54 +0530

    Added code

--- vars/commitinfo.groovy
+++ vars/commitinfo.groovy
@@ -1,11 +1,15 @@
 #!/usr/bin/env groovy
 
 def call () {
- Map<String,String> info = new HashMap<>();
- info.put("message",sh (
-script:"git log --pretty=%s -1",
-returnStdout:true).trim()
- )   
+    env["GIT-MESSAGE"]= sh (
+    script:"git log --pretty=%s -1",
+    returnStdout:true).trim()
 
- return info
+    env["GIT-COMMIT"]= sh (
+    script:"git rev-parse HEAD",
+    returnStdout:true).trim()
+
+    env["GIT-AUTHOR"]= sh (
+    script:"git --no-pager show -s --format='%an' ${GIT-COMMIT}",
+    returnStdout:true).trim()
 }
