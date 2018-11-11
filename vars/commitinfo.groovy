a821cb51140224526cc4c0aec39bc44562bb5fe1
Ashwani Kumar <ashwanikumar04@gmail.com>
2018-11-11 10:44:36 +0530

    Git info

--- vars/commitinfo.groovy
+++ vars/commitinfo.groovy
@@ -15,10 +15,15 @@ def call (String dummy) {
     script:"git --no-pager show -s --format='%an' ${gitCommit}",
     returnStdout:true).trim()
 
+    email = sh (
+    script:"git --no-pager show -s --format='%ae' ${gitCommit}",
+    returnStdout:true).trim()
+
     Map<String,String> map = new HashMap<>()
     map.put("message", message)
     map.put("commit", gitCommit)
     map.put("shortCommit", shortGitCommit)
     map.put("author", author)
+    map.put("email", email)
     return map
 }
